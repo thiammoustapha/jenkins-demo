b858b99edb81ca317a63a83097d536c887194d93
Moustapha Thiam <moustaphathiam888@gmail.com>
2021-01-07 10:17:01 +0000

    Second commit

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,10 +1,19 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+
 pipelineJob('theme-park-job') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/thiammoustapha/jenkins-spring-gradle.git'
+                        url 'https://github.com/thiammoustapha/jenkins-demo.git'
                     }
                     branch 'main'
                 }
